4f9169e423d16d2f42b6fe851c989a63063c9854
newalex1985 <newalex1985@tut.by>
2019-03-21 22:17:18 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    var coinsNumbers = [50, 25, 10, 5, 1];
+    var coinsNames = ["H", "Q", "D", "N", "P"];
+    var amount = currency;
+    var total = {};
+
+    if (amount <= 0) {
+        return {};
+    } else if (amount > 10000) {
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+    } else {
+
+        for (var i = 0; i < 5; i++) {
+            if (amount / coinsNumbers[i] >= 1) {
+                total[coinsNames[i]] = (amount - amount % coinsNumbers[i]) / coinsNumbers[i];
+                amount = amount % coinsNumbers[i];
+            }
+            if (amount == 0) {
+                return total;
+            }
+        }
+
+    }
+
 }
